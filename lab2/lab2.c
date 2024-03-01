fe7c205720d5af666e7279fce50fdb126fd06314
Gaurav Agarwal <gaurav@Gauravs-MacBook-Air.local>
2024-03-01 14:52:55 -0500

    new

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -245,8 +245,8 @@ int main()
             printf("%c", keystate[i]);
         }
         for (int i = 0; i <= 130; i++){
-          SENDbuff[i] = " ";
-          keystate[i] = " ";
+          SENDbuff[i] = "\0";
+          keystate[i] = "\0";
         }
         size = 0;
       }
