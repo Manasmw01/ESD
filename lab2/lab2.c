2430b74d6698601575166ccf132173fb5478b4c0
Gaurav Agarwal <gaurav@Gauravs-MacBook-Air.local>
2024-03-01 17:51:06 -0500

    new

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -328,8 +328,8 @@ int main()
             keystate[i-1] = keystate[i];          
           }
           printf("%s", keystate);
-          // keystate[size-1] = ' ';
-          // SENDbuff[size-1] = ' ';
+          keystate[size] = ' ';
+          SENDbuff[size-1] = ' ';
           size--;
         if(size <64){
         fbputs(keystate, rows, 0);
