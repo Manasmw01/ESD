fc188f45e8bdbb9ab5b17ec89775df1f7ad74d62
Manasmw01 <manas.pange@somaiya.edu>
2024-02-29 20:30:15 -0500

    Update lab2.c

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -210,7 +210,7 @@ int main()
             // SENDbuff[cursor] = c;
           }
           size--;
-        SENDbuff[cursor] = "";
+        // SENDbuff[cursor] = "";
         }
         else
         {
