809f7efb54813a2c4809ff5d8f1c44f07614a98b
Gaurav Agarwal <gaurav@Gauravs-MacBook-Air.local>
2024-03-20 14:58:50 -0400

    fixed

--- lab3-sw/hello.c
+++ lab3-sw/hello.c
@@ -83,8 +83,8 @@ int main()
 
   //x-coordinate most sigficant 20
   //y-coordinate max is 15
-  int MAX_Y = 14;
-  int MAX_X = 19;
+  int MAX_Y = 640;
+  int MAX_X = 480;
   coordinates.x = 50;
   coordinates.y = 50;
   int incy = 1;
