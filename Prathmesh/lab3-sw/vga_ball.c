e49101a85f83816e4cb0867a5f161aae73534bdd
Gaurav Agarwal <gaurav@Gauravs-MacBook-Air-2.local>
2024-05-14 01:10:07 -0400

    reverted

--- Prathmesh/lab3-sw/vga_ball.c
+++ Prathmesh/lab3-sw/vga_ball.c
@@ -61,9 +61,9 @@ static void read_samples(audio_samples_t *samples)
 	dev.samples = *samples;
 }
 
-static uint32_t read_samples_simple(void)
+static int read_samples_simple(void)
 {
-	uint32_t output = ioread32(L_SAMPLES(dev.virtbase));
+	int output = ioread32(L_SAMPLES(dev.virtbase));
 	return output;
 }
 
