4707d418a7526f44009b4679e2eedacb70c4e407
Gaurav Agarwal <gaurav@Gauravs-MacBook-Air-2.local>
2024-05-15 04:29:54 -0400

    push

--- software/test2.cpp
+++ software/test2.cpp
@@ -379,7 +379,7 @@ int main()
 		addToBuffer(buffer, amt.data);
 	
 		if(buffer.size() == N){
-			printf("buffer: %d", buffer[0]);
+			printf("buffer: %d\n", buffer[0]);
 			for (int i = 0; i < N; i++){
 				signal[i].real = buffer[i];
         		signal[i].imag = 0;
