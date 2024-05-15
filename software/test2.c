3e758368e25046840bed8b4d97eaeb0d619a0440
Gaurav Agarwal <gaurav@Gauravs-MacBook-Air-2.local>
2024-05-14 21:33:43 -0400

    removed right signal

--- software/test2.c
+++ software/test2.c
@@ -276,10 +276,10 @@ int main()
 			return 1;
 	}
 	int iter = 0;
-	while (iter < 44100) {
+	while (iter < 1000) {
 		amt.data = get_aud_data(aud_fd);
 		printf("AUD DATA: %d\n", amt.data);
-		fprintf(fptr, "%d", amt.data);
+		fprintf(fptr, "%d\n", amt.data);
 		iter++;
 	}
 		
