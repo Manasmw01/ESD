81ad7cb1f644d7f186f8d689acaf924291c3ec0a
Manasmw01 <manas.pange@somaiya.edu>
2024-05-15 16:55:54 -0400

    SOFTWARE ADDED SPRITES

--- software/test2.cpp
+++ software/test2.cpp
@@ -343,10 +343,10 @@ int main()
  	//FILE *fp = fopen("test.txt", "w");
 	//if (fp == NULL)	return -1;
 	
-	// sprite *sprites = NULL;	
-	// sprites = calloc(SIZE, sizeof(*sprites));
-	// screen_refresh(sprites);
-	// scorecombosetup(sprites);
+	sprite *sprites = NULL;	
+	sprites = calloc(SIZE, sizeof(*sprites));
+	screen_refresh(sprites);
+	scorecombosetup(sprites);
 	Complex signal[N];
 
 	int score = 0;
@@ -371,7 +371,28 @@ int main()
 	int iter = 0;
 	
 	while (1) {
+
+		if ((counter%10)==0) {
+		    gamecounter++;
+		    printf("%d\n", gamecounter);
+		}
+
+		if ((counter%132)==0) {
+		    if(noteCount < MAX_NOTE_COUNT) 
+		    	spawnnote(sprites, (rand() % 5));
+		    noteCount++; 
+		}
+		
+		validleft = check_valid_region(sprites, 23);
+		validright = check_valid_region(sprites, validleft+1);
+
+
 		amt.data = get_aud_data(aud_fd);
+		if (sprites[validleft].y == 399) {
+		    combo_flag = 0;
+		    combo = 0;
+	    }
+
 		//printf("AUD DATA: %d\n", amt.data);
 		addToBuffer(buffer, amt.data);
 	
@@ -403,7 +424,52 @@ int main()
 			// printf("The note played is: %.2f Hz, which is approximately %dth note on a piano.\n", frequency);
 			printf("%.2f Hz\n", frequency);
 		}
+
 		fprintf(fptr, "%d\n", amt.data);
+		if ((amt.data == (1+(sprites[validleft].id-17)>>1)) && (sprites[validleft].id!=0)) {
+		    hitcount++;
+		    // sprites[validleft].y = 481;
+		    // sprites[validright].y = 481;
+		    if (hitcount == 8) {
+		        hitcount = 0;
+		        sprites[validleft].hit = 1;
+		        sprites[validright].hit = 1;
+		        sprites[validleft].dy = 0;
+		        sprites[validright].dy = 0;
+		        sprites[validleft].dx = 10;
+		        sprites[validright].dx = 10;
+		        
+		        if (combo_flag) {
+		            combo++;
+		            if (combo > max)
+		                max = combo;
+		        }
+		        score = score + 1 + combo/5;
+		    }
+ 		}
+ 	    else 
+ 	        hitcount = 0;
+ 		
+ 		//update_score(sprites, amt.data);
+		//update_combo(sprites, 1+(sprites[validleft].id-17)>>1);
+		update_score(sprites, score);
+		update_combo(sprites, combo);
+		update_max(sprites, max);
+	
+		//package the sprites together
+		for (int i = 0; i < SIZE; i++) {
+			vzdt.data[i] = (sprites[i].index<<26) + (sprites[i].id<<20) + (sprites[i].y<<10) + (sprites[i].x<<0);
+		}
+		//send package to hardware
+		send_sprite_positions(&vzdt, vga_zylo_fd);
+		//update spirtes x and y based on dx and dy on software side
+		for (int i = 0; i < SIZE; i++) {
+			updateBall(&sprites[i]);
+		}
+		combo_flag = 1;
+		//pause to let hardware catch up
+		counter++;
+
 		iter++;
 	}
 		
